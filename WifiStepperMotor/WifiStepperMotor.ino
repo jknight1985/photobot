1d4fdd705a12737f3ca10784daa73b1e9cdb0964
Joern Kiwitt <joern.kiwitt@googlemail.com>
2017-03-11 18:41:19 +0100

    v3

--- WifiStepperMotor/WifiStepperMotor.ino
+++ WifiStepperMotor/WifiStepperMotor.ino
@@ -2,18 +2,17 @@
 #include <Stepper.h>
 
 WiFiServer server(80); //Initialize the server on Port 80
-unsigned char SENS_PIN = A0;
 
 // Stepper motor
-int SPMU = 32;  
-Stepper myStepper(SPMU, 2,3,4,5);  
-
+int SPMU = 2048;  // Steps per revolution
+int StartMotor = 0; 
+Stepper myStepper(SPMU,D3,D2,D1,D0);  
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-    //Serial.begin(9600);
-  
+    Serial.begin(9600);
+
   // Start server
     WiFi.mode(WIFI_AP); //Our ESP8266-12E is an AccessPoint
     WiFi.softAP("Photobot", "12345678"); // Provide the (SSID, password);
@@ -21,16 +20,17 @@ void setup() {
 
   // Looking under the hood
     Serial.begin(115200); //Start communication between the ESP8266-12E and the monitor window
-    IPAddress HTTPS_ServerIP= WiFi.softAPIP(); // Obtain the IP of the Server
-    Serial.print("Server IP is: "); // Print the IP to the monitor window
-    Serial.println(HTTPS_ServerIP);
+    //IPAddress HTTPS_ServerIP= WiFi.softAPIP(); // Obtain the IP of the Server
+    //Serial.print("Server IP is: "); // Print the IP to the monitor window
+    //Serial.println(HTTPS_ServerIP);
 
   // Stepper motor setup
-  myStepper.setSpeed(800);
+  myStepper.setSpeed(5);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+
   WiFiClient client = server.available();
   if (!client) {
     return;
@@ -43,20 +43,19 @@ void loop() {
   //Looking under the hood
     Serial.println(request);  
   
-  if (request.indexOf("/") != -1){
-    //TODO
+    if (request.indexOf("/OFF") != -1){
+      StartMotor=0; 
     }
-
-  // Buttons document to html browser
+    else if (request.indexOf("/ON") != -1){
+      StartMotor=1;
+    }
+  // Buttons document to hmtl browser
   String s = "HTTP/1.1 200 OK\r\n";
-  s +="Content-Type: text/html,\r\n\r\n";  
-  s += "<meta http-equiv=\"refresh\" content=\"1\">"; // Update every 5 seconds
-  s +="<body>Light: "; 
-  s +=String('bla');
-  s += "</body>"; 
-  s +="<br><input type=\"button\" name =\"b1\" value=\"Update\" onclick=\"location.href='/ON'\">";
+  s +="Content-Type: text/html,\r\n\r\n";   
   s +="<!DOCTYPE html>\r\n<html>\r\n";
+  s +="<br><input type=\"button\" name =\"b1\" value=\"Turn Motor ON\" onclick=\"location.href='/ON'\">";
   s +="<br><br><br>";
+  s +="<br><input type=\"button\" name =\"b1\" value=\"Turn Motor OFF\" onclick=\"location.href='/OFF'\">";
   s +="</html>\n";
 
   //Serve the HTML document to the browser.
@@ -65,11 +64,13 @@ void loop() {
   delay(1);
   Serial.println("Client disonnected"); //Looking under the hood  
 
-  // Stepper motor test
-  myStepper.step(600);   // eine Umdrehung
-    delay(500);
-    myStepper.step(-600);  // wieder rückwarts
-    delay(500);
 
-  
+  // Stepper motor test
+ if(StartMotor)
+ {
+  myStepper.step(500);   // eine Umdrehung
+  delay(1000);
+  myStepper.step(-500);   // eine Umdrehung 
+  delay(1000); 
+ }
 }
